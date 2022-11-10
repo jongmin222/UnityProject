d9b95c8eb7aed15b08384565e3fe18580e31ec80
jongmin2 <min980518@gmail.com>
2022-11-11 03:48:33 +0900

    이펙트 까지 추가
    이펙트 까지 추가

--- My project (1)/Assets/Scripts/CharaAnimation.cs
+++ My project (1)/Assets/Scripts/CharaAnimation.cs
@@ -7,52 +7,52 @@ public class CharaAnimation : MonoBehaviour
 	Vector3 prePosition;
 	bool isDown = false;
 	bool attacked = false;
-	
+
 	public bool IsAttacked()
 	{
 		return attacked;
 	}
-	
+
 	void StartAttackHit()
 	{
-		Debug.Log ("StartAttackHit");
+		Debug.Log("StartAttackHit");
 	}
-	
+
 	void EndAttackHit()
 	{
-		Debug.Log ("EndAttackHit");
+		Debug.Log("EndAttackHit");
 	}
-	
+
 	void EndAttack()
 	{
 		attacked = true;
 	}
-	
-	void Start ()
+
+	void Start()
 	{
 		animator = GetComponent<Animator>();
 		status = GetComponent<CharacterStatus>();
-		
+
 		prePosition = transform.position;
 	}
-	
-	void Update ()
+
+	void Update()
 	{
 		Vector3 delta_position = transform.position - prePosition;
 		animator.SetFloat("Speed", delta_position.magnitude / Time.deltaTime);
-		
-		if(attacked && !status.attacking)
+
+		if (attacked && !status.attacking)
 		{
 			attacked = false;
 		}
 		animator.SetBool("Attacking", (!attacked && status.attacking));
-		
-		if(!isDown && status.died)
+
+		if (!isDown && status.died)
 		{
 			isDown = true;
 			animator.SetTrigger("Down");
 		}
-		
+
 		prePosition = transform.position;
 	}
 }
